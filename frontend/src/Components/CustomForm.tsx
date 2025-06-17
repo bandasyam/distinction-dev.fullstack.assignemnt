29ec976b31cd8d5a5f3f66a84f5fa50b6d0cdda6
banda syam <bandasyam12@gmail.com>
2025-06-17 21:44:30 +0530

    Update CustomForm.tsx

--- frontend/src/Components/CustomForm.tsx
+++ frontend/src/Components/CustomForm.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, TextField } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { use, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { callApi, ip } from "../utils/api.helpers";
 
@@ -22,6 +22,8 @@ export default function CustomForm({ formType, productData }: CustomFormProps) {
   const [name, setName] = useState(productData?.name);
   const [price, setPrice] = useState(productData?.price);
   const [tags, setTags] = useState(productData?.tags);
+
+  const [errorMessage, setErrorMessage] = useState({ name: "", price: "", tags: "" });
 
   async function handleSubmitClick() {
     try {
@@ -51,11 +53,46 @@ export default function CustomForm({ formType, productData }: CustomFormProps) {
     }
   }
 
+  function handleNameChange(value: any) {
+    setName(value);
+  }
+
+  function handlePriceChange(value: any) {
+    console.log(value);
+    if (value == "") {
+      return;
+    }
+
+    if (isNaN(Number(value))) {
+      return setErrorMessage((prev) => ({ ...prev, price: "Price should be a number" }));
+    } else {
+      return setPrice(Number(value));
+    }
+  }
+
   return (
     <form>
-      <TextField label="Name" variant="outlined" fullWidth margin="normal" value={name} onChange={(e) => setName(e.target.value)} />
-      <TextField label="price" variant="outlined" fullWidth margin="normal" value={price} onChange={(e) => setPrice(Number(e.target.value))} />
-      <TextField label="tags" variant="outlined" fullWidth margin="normal" value={tags} onChange={(e) => setTags(e.target.value.split(","))} />
+      <div>
+        <TextField label="Name" variant="outlined" fullWidth margin="normal" value={name} onChange={(e) => handleNameChange(e.target.value)} />
+      </div>
+
+      <div>
+        <TextField label="price" variant="outlined" fullWidth margin="normal" value={price} onChange={(e) => handlePriceChange(e.target.value)} />
+        {errorMessage?.price ? <Typography color="red">{errorMessage?.price}</Typography> : null}
+      </div>
+
+      <div>
+        <TextField
+          label="tags"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          placeholder="Enter tags separated by commas (e.g., electronics, phone, keyboard)"
+          value={tags}
+          onChange={(e) => setTags(e.target.value.split(","))}
+        />
+      </div>
+
       <Box display="flex" justifyContent="center" mt={2}>
         <Button variant="contained" color="inherit" type="submit" onClick={handleSubmitClick}>
           Submit
